1ad512011083789b5f52c05e4871034fdb4ce1cd
Paul Kiat <pk204@student.london.ac.uk>
2024-03-29 13:41:23 -0700

    added header/footer components

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* Other components like Route components go here */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>BeautifulParent.com</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/shop">Shop</a></li>
+          <li><a href="/cart">Cart</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
